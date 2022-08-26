60fc881732cede2f2c9bc792ad891584d462eb6d
ermias <you@example.com>
2022-08-26 19:34:25 +0300

    update

--- client/src/components/Avatar.jsx
+++ client/src/components/Avatar.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
 const Avatar = () => {
+  const user = JSON.parse(localStorage.getItem("profile"));
+  const userName = user?.result?.name;
+  let userInitials = "";
+  if (userName) {
+    if (userName.includes(" ")) {
+      let names = userName.split(" ");
+      userInitials = `${names[0].slice(0, 1)}${names[1].slice(0, 1)}`;
+    } else {
+      userInitials = userName.slice(0, 1);
+    }
+  }
+
   return (
     <div className="inline-flex items-center justify-center w-8 h-8 text-sm text-white bg-indigo-500 rounded-full">
-      EB
+      {userInitials}
     </div>
   );
 };
